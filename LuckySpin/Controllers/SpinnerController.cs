e09ac2460d10b5a4fd026e6e83829df2860765e6
Micah Cave <micahcave7@gmail.com>
2022-01-11 20:39:59 -0800

    I'm trying to work on it still and I think I'm getting closer to finishing but I can't compile. I sent a message in Canvas to you with the error.

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -8,9 +8,12 @@ namespace LuckySpin.Controllers
         // TODO: Modify the Index Action to return Lucky.Output as the Content
         public IActionResult Index(LuckyNumber Lucky, int luck)
         {
-            //TODO: Receive the Routing id as a parameter called "luck"
-            //TODO: Set LuckyNumber object's Luck property to the value of "luck" before returning the ContentResult
+            
             return new ContentResult { Content = Lucky.Output, ContentType = "text/html" };
+            
+            //TODO: Receive the Routing id as a parameter called "luck"
+            //TODO: Set LuckyNumber object's Luck property to the value of "luck"
+            //before returning the ContentResult
             return new ContentResult { Content = "<h1>We're Ready to Spin</h1>", ContentType = "text/html" };
         }
     }
